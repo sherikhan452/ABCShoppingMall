ffad09c863873c2fcc2122a421e512110a61345f
sheheryar <sherikhan451@gmail.com>
2024-05-19 22:08:25 +0500

    Final Till Galleries

---
+++ ABCShoppingMall/Controllers/GalleriesUserController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ABCShoppingMall.Data;
+using ABCShoppingMall.Models;
+
+namespace ABCShoppingMall.Controllers
+{
+    public class GalleriesUserController : Controller
+    {
+        private ABCShoppingMallContext db = new ABCShoppingMallContext();
+
+        // GET: GalleriesUser
+        public ActionResult Index()
+        {
+            var images = db.Galleries.ToList();
+            ViewBag.ImageId = images;
+
+            return View(images);
+        }
+
+        public ActionResult DisplayImage(int id)
+        {
+
+            //var image = db.ShoppingCenters.Find(id);
+            var image = db.Galleries.FirstOrDefault(x => x.Id == id);
+
+            return File(image.Image, "image/jpeg");
+        }
+
+
+        // GET: GalleriesUser/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Galleries.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // GET: GalleriesUser/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GalleriesUser/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,GalleryName,Gallery_Detail,Image")] Gallery gallery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Galleries.Add(gallery);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gallery);
+        }
+
+        // GET: GalleriesUser/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Galleries.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // POST: GalleriesUser/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,GalleryName,Gallery_Detail,Image")] Gallery gallery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gallery).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gallery);
+        }
+
+        // GET: GalleriesUser/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Galleries.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // POST: GalleriesUser/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gallery gallery = db.Galleries.Find(id);
+            db.Galleries.Remove(gallery);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
